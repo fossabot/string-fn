310b0dfc3ba69969eac55eff1ee11c75504dd1fc
Dejan Toteff <deyan8284@gmail.com>
2019-04-20 15:52:15 +0300

    test: standalone constant case

--- src/standalone/constantCase.spec.js
+++ src/standalone/constantCase.spec.js
@@ -1,8 +1,14 @@
 import { constantCase } from './constantCase'
 
-test('', () => {
-  const result = constantCase('fooBarBAZ')
-  const expectedResult = 'FOO_BAR_BAZ'
-  
-  expect(result).toEqual(expectedResult)
+const expectedResult = 'FOO_BAR_BAZ'
+test('happy', () => {
+  expect(constantCase('foo BarBAZ')).toEqual(expectedResult)
+})
+
+test('from camel case', () => {
+  expect(constantCase('fooBarBaz')).toEqual(expectedResult)
+})
+
+test('from dot case', () => {
+  expect(constantCase('foo.bar.baz')).toEqual(expectedResult)
 })
